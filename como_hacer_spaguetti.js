8a2fe4adab8f4f2baead86d2c263b7cd1e3a0c34
alberto salmeron <jesusasalmeron2006@gmail.com>
2016-09-24 00:16:37 -0400

    Add files via upload

---
+++ como_hacer_spaguetti.js
@@ -0,0 +1,51 @@
+function miFuncion()
+{
+	var recipiente;
+	while(recipiente!="olla"){
+	recipiente = prompt("coloca un recipiente preferiblemente una olla!!!")
+	}
+    var respuesta;
+    while(respuesta!="agua"){
+	respuesta = prompt("agrega agua")
+	}
+     alert("haz agregado " + respuesta + " en una " + recipiente);
+    } 
+
+    
+    function sabor()
+    {
+		var salar;
+	while(salar!="sal"){
+	salar = prompt("agrega sal")
+	}
+    var aceitar;
+    while(aceitar!="aceite"){
+	aceitar = prompt("agrega aceite")
+	}
+     alert("haz agregado " + salar + " y " + aceitar);
+    } 
+		
+function spaguetti()
+    {
+		var pasta;
+	while(pasta!="spaguetti"){
+	pasta = prompt("agrega spaguetti")
+	}
+    alert("haz agregado " + pasta + " ahora espera que este listo "); 
+    var embase;
+    while(embase!="plato"){
+		embase=prompt("tome un plato")
+		}
+    var cubierto;
+    while(cubierto!="tenedor"){
+		cubierto= prompt("tome un utensilio preferiblemente tenedor!!")
+		}
+    function example(){
+       alert("esta listo el "+ pasta + " bon apetit!!!! profesor hasta aqui es mi algoritmo");
+       
+     }
+    setTimeout(example,10000);
+
+}
+
+   
